36d67cbc7e360843f1c242c3f43533ba84f499fc
bharat1910 <bharat1910@gmail.com>
2013-11-26 20:07:26 -0600

    Committing the structure for the code folder

---
+++ code/GenerateModifiedFileJsHInt.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class GenerateModifiedFileJsHInt {
+	
+	private void run() throws IOException
+	{
+		File root = new File("src");
+		BufferedReader br;
+		
+		for (File f : root.listFiles()) {
+			
+			for (File ff : f.listFiles()) {
+				br = new BufferedReader(new FileReader(ff.getAbsolutePath()));
+				
+				br.close();
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		GenerateModifiedFileJsHInt	main = new GenerateModifiedFileJsHInt();
+		main.run();
+		System.exit(0);
+	}
+}
